71ff6066e14c6e6318326316158f475bd8b0877b
murollo <murillo4493@gmail.com>
2021-03-18 20:47:19 -0300

    mais 2 pra conta

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -1,14 +1,14 @@
+//a classe TesteEmpregado ficou assim
 public class TesteEmpregado{
-    public static void main (String [] args){
-    Empregado e1 = new Empregado ();
-    Empregado e2 = new Empregado();
-    Empregado e3 = new Empregado ();
-    //e agora, como configurar o tipo deles?
-    }
-public void setTipo (int tipo){
-    this.tipo = tipo;
-    }
-    public int tipo (){
-    return this.tipo;
-    }
-}
+public static void main (String [] args){
+Empregado e1 = new Empregado ();
+Empregado e2 = new Empregado();
+Empregado e3 = new Empregado ();
+//configurando tipo dos empregados
+e1.setTipo(1);
+e2.setTipo(2);
+e3.setTipo(3);
+//configurando alguns valores
+e1.setSalario(2000);
+e2.setSalario(1700);
+e2.setComissao (0.2);//20% de comissão sobre o salário
