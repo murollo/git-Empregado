6cec177db541002d5efd00f3c9ec7ab7cc42ec1d
murollo <murillo4493@gmail.com>
2021-03-19 20:05:15 -0300

    colocando os .java

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -1,3 +1,4 @@
+import java.util.*;
 //a classe TesteEmpregado ficou assim
 public class TesteEmpregado{
 public static void main (String [] args){
@@ -8,7 +9,19 @@ Empregado e3 = new Empregado ();
 e1.setTipo(1);
 e2.setTipo(2);
 e3.setTipo(3);
-//configurando alguns valores teste teste
+//configurando alguns valores
 e1.setSalario(2000);
 e2.setSalario(1700);
 e2.setComissao (0.2);//20% de comissão sobre o salário
+e3.setSalario(1500);
+e3.setBonus(400);
+//adicionando todo mundo em uma lista
+List <Empregado> emps = Arrays.asList(e1, e2, e3);
+//mostra os valores
+/*System.out.println (e1.calculaSalario());
+System.out.println (e2.calculaSalario());
+System.out.println (e3.calculaSalario());*/
+for (Empregado e : emps)
+System.out.println (e.calculaSalario());
+}
+}
