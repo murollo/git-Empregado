93923dfdd310cd9986af9376fef99557f4d681a2
murollo <murillo4493@gmail.com>
2021-03-20 15:57:40 -0300

    Processamento de empregados usando lista

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -1,3 +1,4 @@
+import java.util.*;
 //a classe TesteEmpregado ficou assim
 public class TesteEmpregado{
 public static void main (String [] args){
@@ -14,9 +15,13 @@ e2.setSalario(1700);
 e2.setComissao (0.2);//20% de comissão sobre o salário
 e3.setSalario(1500);
 e3.setBonus(400);
+//adicionando todo mundo em uma lista
+List <Empregado> emps = Arrays.asList(e1, e2, e3);
 //mostra os valores
-System.out.println (e1.calculaSalario());
+/*System.out.println (e1.calculaSalario());
 System.out.println (e2.calculaSalario());
-System.out.println (e3.calculaSalario());
+System.out.println (e3.calculaSalario());*/
+for (Empregado e : emps)
+System.out.println (e.calculaSalario());
 }
 }
